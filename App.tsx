239ccb2249499fc407288bc63fc187a3616b028e
bandinuguri <bandinuguri@gmail.com>
2026-02-06 16:25:57 +0900

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -138,7 +138,7 @@ const App: React.FC = () => {
             <Plane size={24} fill="currentColor" />
           </div>
           <div className="header-info">
-            <h1>전국 공항 실시간 기상</h1>
+            <h1>공항 기상 정보</h1>
             <span className="update-time">
               {lastUpdatedStr}
             </span>
@@ -162,7 +162,7 @@ const App: React.FC = () => {
       <div className="info-banner">
         <Info size={18} />
         <span>
-        최신정보 : 아래 공항 클릭( <a href="https://amo.kma.go.kr/" target="_blank" rel="noreferrer" className="info-link">항공기상청</a>, <a href="https://www.weather.go.kr/" target="_blank" rel="noreferrer" className="info-link">날씨누리</a>, <a href="https://www.weather.go.kr/w/special-report/overall.do" target="_blank" rel="noreferrer" className="info-link">특보</a> )
+        최신정보: 공항명 클릭( <a href="https://amo.kma.go.kr/" target="_blank" rel="noreferrer" className="info-link">항공기상청</a>, <a href="https://www.weather.go.kr/" target="_blank" rel="noreferrer" className="info-link">날씨누리</a>, <a href="https://www.weather.go.kr/w/special-report/overall.do" target="_blank" rel="noreferrer" className="info-link">특보</a> )
         </span>
       </div>
 
